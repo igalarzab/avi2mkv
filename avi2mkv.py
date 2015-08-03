c80940c2b6c03e9ca9b0665c70a1c6515c52b7c1
Jose Ignacio Galarza <igalarzab@gmail.com>
2015-08-03 22:19:20 +0200

    Release new version

--- avi2mkv.py
+++ avi2mkv.py
@@ -29,7 +29,7 @@ from optparse import OptionParser
 # Global information
 __uname__ = 'avi2mkv'
 __long_name__ = 'Simple AVI/MP4 to MKV converter'
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = 'Jose Ignacio Galarza'
 __email__ = 'igalarzab@gmail.com'
 __url__ = 'http://github.com/igalarzab/avi2mkv'
